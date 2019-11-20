19e48b22e53ad4fb04fde30de295d2758d4c3907
gerard.lucas <gerard.lucas@conperio.com>
2019-11-20 17:23:05 -0600

    testing some js stuff

--- StreamingTvComparison/wwwroot/js/site.js
+++ StreamingTvComparison/wwwroot/js/site.js
@@ -2,6 +2,7 @@
 function setupChannelTable() {
     $('#tblChannels').bootstrapTable({
         pagination: true,
+        pageList: [25, 50, 100, 'All'],
         search: true,
         sortable: true,
         showColumns: true
